472053aa9aef4a466519741fcdf1fb4926d3a7a9
Carlos Pinto <ikirux@gmail.com>
2014-04-01 17:33:09 -0300

    Carga inicial del proyecto

---
+++ DatePickerControl.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * DatePickerControl widget class
+ *
+ * @author: Carlos Pinto <ikirux@gmail.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ *
+ */
+class DatePickerControl extends CInputWidget
+{
+	/**
+	 * @var array the options for the Bootstrap JavaScript plugin.
+	 */
+	public $options = [];
+
+	/**
+	 * @var string[] the JavaScript event handlers.
+	 */
+	public $events = [];
+
+	/**
+	 * Initializes the widget.
+	 */
+	public function init()
+	{
+		$this->htmlOptions['type'] = 'text';
+		$this->htmlOptions['autocomplete'] = 'off';
+
+		if (!isset($this->options['language'])) {
+			$this->options['language'] = Yii::app()->language;
+		}
+
+		if (!isset($this->options['format'])) {
+			if (!isset(Yii::app()->params['dateFormat'])) {
+				$this->options['format'] = 'yyyy/mm/dd';
+			} else {
+				$this->options['format'] = Yii::app()->params['dateFormat'];
+			}
+		} 
+		
+		if (!isset($this->options['weekStart'])) {
+			$this->options['weekStart'] = 0; // Sunday
+		}
+	}
+
+	/**
+	 * Runs the widget.
+	 */
+	public function run()
+	{
+		list($name, $id) = $this->resolveNameID();
+
+		if ($this->hasModel()) {
+			echo BsHtml::activeTextFieldControlGroup($this->model, $this->attribute, $this->htmlOptions);
+		} else {
+			echo BsHtml::activeTextFieldControlGroup($name, $this->value, $this->htmlOptions);
+		}
+
+		$this->registerClientScript($id);
+	}
+
+	/**
+	 * Registers required client script for bootstrap datepicker. It is not used through bootstrap->registerPlugin
+	 * in order to attach events if any
+	 */
+	public function registerClientScript($id)
+	{
+		$baseScriptUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('ven.eternicode.bootstrap-datepicker'));
+
+		$cs = Yii::app()->getClientScript();
+		$cs->registerCssFile($baseScriptUrl . '/css/datepicker3.css');
+		$cs->registerScriptFile($baseScriptUrl . '/js/bootstrap-datepicker.js', CClientScript::POS_END);
+
+		$options = !empty($this->options) ? CJavaScript::encode($this->options) : '';
+
+		ob_start();
+		echo "jQuery('#{$id}').datepicker({$options})";
+		foreach ($this->events as $event => $handler) {
+			echo ".on('{$event}', " . CJavaScript::encode($handler) . ")";
+		}
+
+		Yii::app()->getClientScript()->registerScript(__CLASS__ . '#' . $this->getId(), ob_get_clean() . ';');
+	}
+}
