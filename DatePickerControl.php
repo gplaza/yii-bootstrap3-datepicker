ab68d2114ef4c1d30aa78105cb560e5257eced45
Carlos Pinto <ikirux@gmail.com>
2014-05-04 00:21:51 -0400

    Se cambia el funcionamiento, ahora la extension es capaz de generar el codigo del campo de manera automatica, tambien se agrega el soporte para utilizar los eventos de la extension, ademas de poder realizar triggers de los eventos. Tambien se implemenrta la posibilidad de cargar el calendario de forma embebida

--- DatePickerControl.php
+++ DatePickerControl.php
@@ -19,13 +19,33 @@ class DatePickerControl extends CInputWidget
 	public $events = [];
 
 	/**
+	 * @var string the date picker type (text and embedded are supported).
+	 */
+	public $type = 'text';
+
+	/**
+	 * @var string[] triggers the specifics events after the date picker is load
+	 */
+	public $triggerEvents = [];
+
+	/**
+	 * @var string[] call the specified methods
+	 * 
+	 * format:
+	 *
+	 *  [
+	 *		'name' => 'someName',
+	 *		'arg1' => 'arg1',
+	 *		'arg2' => 'arg2',
+	 *  ]
+	 */
+	public $methods = [];
+
+	/**
 	 * Initializes the widget.
 	 */
 	public function init()
 	{
-		$this->htmlOptions['type'] = 'text';
-		$this->htmlOptions['autocomplete'] = 'off';
-
 		if (!isset($this->options['language'])) {
 			$this->options['language'] = Yii::app()->language;
 		}
@@ -50,7 +70,24 @@ class DatePickerControl extends CInputWidget
 	{
 		list($name, $id) = $this->resolveNameID();
 
-		$this->registerClientScript($id);
+		if ($this->type == 'embedded') {
+			$idEmbeddedContainer = $id . '_container';
+			echo CHtml::tag('div', ['id' => $idEmbeddedContainer], false, false);
+			echo CHtml::activeHiddenField($this->model, $this->attribute);
+			$this->events['changeDate'] = 'js:function(e)  {
+                dates = $("#' . $idEmbeddedContainer . '").datepicker(\'getFormattedDate\', \'' . $this->options['format'] . '\');
+                $("#' . $id . '").val(dates);
+            }';
+   			echo CHtml::closeTag('div');
+			$this->registerClientScript($idEmbeddedContainer);
+		} else { // default 'text'
+			echo CHtml::tag('div', ['class' => 'form-group'], false, false);
+			echo CHtml::activeLabel($this->model, $this->attribute);
+			echo CHtml::tag('div', [], false, true);
+			echo CHtml::activeTextField($this->model, $this->attribute, ['class' => 'form-control']);
+			echo CHtml::closeTag('div');
+			$this->registerClientScript($id);
+		}
 	}
 
 	/**
@@ -65,14 +102,44 @@ class DatePickerControl extends CInputWidget
 		$cs->registerCssFile($baseScriptUrl . '/css/datepicker3.css');
 		$cs->registerScriptFile($baseScriptUrl . '/js/bootstrap-datepicker.js', CClientScript::POS_END);
 
+		// We load the language
+		if ($this->options['language'] != "en") {
+			$cs->registerScriptFile($baseScriptUrl . '/js/locales/bootstrap-datepicker.' . $this->options['language'] . '.js', CClientScript::POS_END);
+		}
+
 		$options = !empty($this->options) ? CJavaScript::encode($this->options) : '';
 
 		ob_start();
 		echo "jQuery('#{$id}').datepicker({$options})";
+		foreach ($this->methods as $method) {
+			$argument = '';
+			if (isset($method['name'])) {
+				$argument = "'{$method['name']}'";
+
+				// the name is required
+				if (isset($method['arg1'])) {
+					$argument .= ", {$method['arg1']}";
+				}
+
+				if (isset($method['arg2'])) {
+					$argument .= ", {$method['arg2']}";
+				}				
+			}	
+			
+			if (!empty($argument)) {
+				echo ".datepicker($argument)";	
+			}
+		}		
 		foreach ($this->events as $event => $handler) {
 			echo ".on('{$event}', " . CJavaScript::encode($handler) . ")";
 		}
 
-		Yii::app()->getClientScript()->registerScript(__CLASS__ . '#' . $this->getId(), ob_get_clean() . ';');
+		echo ';';
+
+		foreach($this->triggerEvents as $event) {
+			echo "jQuery('#{$id}').trigger('{$event}');";	
+		}
+		
+		Yii::app()->getClientScript()->registerScript(__CLASS__ . '#' . $this->getId(), ob_get_clean());
 	}
 }
